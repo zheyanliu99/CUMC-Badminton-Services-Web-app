9d5843af5f9f94c147c966effd5a147f6b32952d
“Zheyan <zheyan.liu@columbia.edu>
2022-10-24 19:04:00 -0400

    add quit waitlist

---
+++ CUMC-Badminton-Service/src/app/sessions-edit/sessionEditInput.ts
@@ -0,0 +1,8 @@
+export class sessionInput {
+    sessionid: number;
+    begintime: string;
+    endtime: string;
+    capacity: number;
+    enrolled: number
+    notes: string;
+  }
--- CUMC-Badminton-Service/src/app/sessions-edit/sessions-edit.component.ts
+++ CUMC-Badminton-Service/src/app/sessions-edit/sessions-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { sessionInput } from './sessionEditInput';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sessions-edit',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SessionsEditComponent implements OnInit {
 
-  constructor() { }
+  sessions: Array<sessionInput>;
+  userId: string;
+
+  constructor(private http:HttpClient) {
+    this.userId = sessionStorage.getItem('userId')
+  }
 
   ngOnInit(): void {
+    this.get_sessions_by_user().subscribe(results => {
+      console.log(results.data)
+      if(results.success){
+        console.log("update data")
+        this.sessions = results.data
+        console.log(this.sessions)
+        }
+      else{
+        alert("Results Not Found")
+      }
+    })
+  }
+
+  quit(sessionid:number): void{
+    console.log('quit')
+    this.delete_from_waitlist(sessionid).subscribe(results => {
+      if(results.success){
+        alert(results.message)}
+      else{
+        alert(results.message)
+      }
+    this.get_sessions_by_user().subscribe(results => {
+      console.log(results.data)
+      if(results.success){
+        console.log("update data")
+        this.sessions = results.data
+        console.log(this.sessions)
+        }
+      else{
+        alert("Results Not Found")
+      }
+    })
+    })
+  }
+
+  get_sessions_by_user():Observable<any>{
+    console.log('renew')
+    return this.http.get<any>(`${environment.ms2Url}/api/session/user/${this.userId}`);
+  }
+
+  delete_from_waitlist(sessionid:number):Observable<any>{
+    return this.http.get<any>(`${environment.ms2Url}/api/session/${sessionid}/quit/${this.userId}`)
   }
 
 }
