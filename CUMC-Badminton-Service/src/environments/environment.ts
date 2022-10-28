1d0e5c108e1ff4638b49ebdff806850dfdc7c7e7
YiruGONG <ygong2832@gmail.com>
2022-10-28 00:39:46 -0400

    cloud ms3

--- CUMC-Badminton-Service/src/environments/environment.ts
+++ CUMC-Badminton-Service/src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   ms1Url: "http://localhost:5010",
   ms2Url: "http://127.0.0.1:5011",
-  // ms3Url: "http://E6156ms3-env.eba-jjys9bmm.us-east-1.elasticbeanstalk.com"
-  ms3Url: "http://localhost:5012/"
+  ms3Url: "http://E6156ms3-env.eba-jjys9bmm.us-east-1.elasticbeanstalk.com"
+  // ms3Url: "http://localhost:5012/"
 };
 
 /*
