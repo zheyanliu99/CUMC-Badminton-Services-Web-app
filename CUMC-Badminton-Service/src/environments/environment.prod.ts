140508b067cd3b369de3ca58152196b1e9636526
YiruGONG <ygong2832@gmail.com>
2022-12-06 23:56:56 -0500

    ng build

--- CUMC-Badminton-Service/src/environments/environment.prod.ts
+++ CUMC-Badminton-Service/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  ms1Url: "http://54.165.154.107:5010",
   ms2Url: "http://54.167.191.86:5011",
-  ms3Url: "E6156ms3-env.eba-jjys9bmm.us-east-1.elasticbeanstalk.com"
+  ms3Url: "E6156ms3-env.eba-jjys9bmm.us-east-1.elasticbeanstalk.com",
+  SMARTY_WEBSITE_KEY: '143512218916840455'
 };
