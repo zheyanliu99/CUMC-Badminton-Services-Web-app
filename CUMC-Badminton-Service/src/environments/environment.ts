9142a63f11ed3b4958bbdc819c48f226262eefd4
“Zheyan <zheyan.liu@columbia.edu>
2022-12-17 09:33:07 -0500

    update composite service and profile

--- CUMC-Badminton-Service/src/environments/environment.ts
+++ CUMC-Badminton-Service/src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
   // ms2Url: "https://r450v9soc3.execute-api.us-east-1.amazonaws.com/stage1",
   ms3Url: "http://localhost:5012/",
   // ms3Url: "https://r450v9soc3.execute-api.us-east-1.amazonaws.com/stage1",
+  cs1Url: "http://localhost:5013/",
   SMARTY_WEBSITE_KEY: '143512218916840455'
 };
 
